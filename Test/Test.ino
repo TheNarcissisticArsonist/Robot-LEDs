1f0fa38e4fce8e64f8ba4b743aa59b51b839f8e4
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2016-02-23 21:49:26 -0500

    Code framework for state machine.

--- Test/Test.ino
+++ Test/Test.ino
@@ -12,6 +12,12 @@ int timeDelay = initialTimeDelay;
 int timeDelayIncrement = 1;
 int minTimeDelay = 25;
 
+enum states {
+  IDLE_STATE,         //Driving around, analog line 2 represents speed
+  SHOOTER_ON_STATE,   //Shooter just got activated, pattern TBD. Analog line 2 could represent shooter wheel speed?
+  SHOOTER_OFF_STATE   //Shooter just got deactivated, pattern TBD. Analog line 2 could represent shooter wheel speed?
+};
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMLEDS, ledPin, NEO_GRB + NEO_KHZ800);
 
 void updateLEDs()
@@ -42,5 +48,21 @@ void setup()
 
 void loop()
 {
+  var state = IDLE_STATE;
+  //Data read from RoboRIO goes here
 
+
+  //State machine from here on out...
+  if(state == IDLE_STATE)
+  {
+    //
+  }
+  else if(state == SHOOTER_ON_STATE)
+  {
+    //
+  }
+  else if(state == SHOOTER_OFF_STATE)
+  {
+    //
+  }
 }
