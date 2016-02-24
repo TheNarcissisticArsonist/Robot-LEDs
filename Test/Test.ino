e21ecd72dbd5b7b8beb1c80ed05ccfc725a5c6b6
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2016-02-24 10:29:29 -0500

    I don't know if we'll use these, but I'm going to fix the names anyways :)

--- Test/Test.ino
+++ Test/Test.ino
@@ -42,14 +42,14 @@ void updateLEDs()
   strip.show();
 }
 
-/*void strip_clear() {
+/*void stripClear() {
   for(int i=0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, strip.Color(0, 0, 0));
   }
   strip.show();
 }
 
-void explosion() {
+void explode() {
   for (int i = 0; i<strip.numPixels(); i++) {
     if (i < 12) {
       strip.setPixelColor(i, strip.Color(255, 0, 0));
